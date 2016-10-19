0b49f6d8e386246fd5d9842b2561a8d1573fcb90
Anti-Dalai-Lama <princeartik@gmail.com>
2016-10-19 21:29:07 +0300

    Task 3 is done and tested

--- Task3.py
+++ Task3.py
@@ -16,37 +16,43 @@ class MatrixSquare(object):
 
 
 class Matrix(object):
-    pseudo_squares = list()
+    squares = list()
 
     def __init__(self, matrix):
         self.matrix = matrix
         self.width = len(matrix[0])
         self.height = len(matrix)
 
-    def get_pseudo_squares(self):
+    def get_squares(self):
         for i in range(0, self.height - 1):  # без прохода последней строки
             for j in range(0, self.width - 1):  # без прохода последнего столбца
                 if (self.matrix[i][j + 1] == self.matrix[i + 1][j]):
                     sq = MatrixSquare(i, j, 2)
-                    i1 = i
-                    j1 = j
-                    while (self.matrix[i1][j1 + 1] == self.matrix[i1 + 1][j1] and i1 < self.height - 2 and j1 < self.width - 2):
-                        i1 += 1
-                        j1 += 1
-                        sq.side += 1
+                    i1 = i + 2
+                    j1 = j + 2
+                    while (i1 < self.height and j1 < self.width ):
+                        sideup = True
+                        for k in range(1, sq.side + 1):
+                            if(self.matrix[i1][j1-k] != self.matrix[i1 - k][j1]):
+                                sideup = False
+                        if(sideup):
+                            i1 += 1
+                            j1 += 1
+                            sq.side += 1
+                        else:
+                            break
                     #добавить проверку на то, является ли матрица вписанной в другую?
-                    self.pseudo_squares.append(sq)
+                    contained_flag = False
+                    for square in self.squares:
+                        if(square.side > sq.side and square.contains(sq)):
+                            contained_flag = True
+                            break
+                    if(not contained_flag):
+                        self.squares.append(sq)
 
-    def print_pseudo_sq(self):
-        for el in self.pseudo_squares:
+    def print_sq(self):
+        for el in self.squares:
             print(el)
-
-    # def check_symmetric_square(self, matrix_square):
-    #     for i in range(matrix_square.row + 1, matrix_square.row + matrix_square.side):
-    #         for j in range(matrix_square.col, matrix_square.col + matrix_square.side):
-    #             #check mirror elements
-
-
 
 
 matrix = [[5,0,7,9,1,2],
@@ -60,8 +66,7 @@ matrix = [[5,0,7,9,1,2],
           [8,3,6,3,1,8]]
 
 mymatrix = Matrix(matrix)
-mymatrix.get_pseudo_squares()
-mymatrix.print_pseudo_sq()
+mymatrix.get_squares()
+mymatrix.print_sq()
 
-print(MatrixSquare(2,2,3).contains(MatrixSquare(3,3,3)))
 
