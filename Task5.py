05c8a0db65cfff27441e7daaba37a6123c231ae5
Anti-Dalai-Lama <princeartik@gmail.com>
2016-10-23 16:14:27 +0300

    Task 5 is improved and commented

--- Task5.py
+++ Task5.py
@@ -3,13 +3,13 @@
 (например, записываются в обратном порядке), далее переставляются сами группы символов."""
 
 
-class ReversedEncryption(object):
+class TurningEncryption(object):
     text = ""
     text_len = 0
-    blocks_sequence = ""
+    blocks_sequence = ""  # sequence of blocks in message
     block_len = 0
-    elements_sequence = ""
-    list_letters = list()
+    elements_sequence = ""  # sequence of elements in block
+    list_letters = list()  # string is being built here
 
     def __init__(self, message, blocks_seq, elements_seq):
         self.text = message
@@ -18,60 +18,51 @@ class ReversedEncryption(object):
         self.elements_sequence = list(map(int, elements_seq.split()))
         self.block_len = len(self.elements_sequence)
 
-    def encrypt(self):
-        self.list_letters.clear()
-        if(self.text_len != len(self.blocks_sequence) * len(self.elements_sequence)):
-            raise Exception("Can't divide message on blocks")
-
-        for gr_num in self.blocks_sequence:
-            for el_num in self.elements_sequence:
-                pos = self.block_len * gr_num + el_num
-                self.list_letters.append(self.text[pos])
-
-        return ''.join(self.list_letters)
-
-    def decrypt(self):
-        self.list_letters.clear()
+        # check if we can divide text into blocks of assigned length
         if (self.text_len != len(self.blocks_sequence) * len(self.elements_sequence)):
             raise Exception("Can't divide message on blocks")
 
-        temp_list = []
-        for i in range(0, self.text_len // self.block_len):
-            temp_list.append(i)
-
-
-        gr_counter = 0
+    def encrypt(self):  #O(n)
+        self.list_letters.clear()
         for gr_num in self.blocks_sequence:
-            temp_list[gr_num] = []
-            el_counter = 0
-            for i in range(0, self.block_len):
-                temp_list[gr_num].append(i)
             for el_num in self.elements_sequence:
-                temp_list[gr_num][el_num] = self.text[gr_counter * self.block_len + el_counter ]
+                pos = self.block_len * gr_num + el_num  # getting a position of current symbol to input in result
+                self.list_letters.append(self.text[pos])  # decided to avoid string concatenation (memory issues)
+        return ''.join(self.list_letters) # getting string from list
+
+    def decrypt(self):  #O()
+        self.list_letters.clear()
+        for i in range(0, self.text_len // self.block_len):  # creating empty list to store blocks and elements [[],[]]
+            temp = list()
+            for k in range(0, self.block_len):
+                temp.append(k)
+            self.list_letters.append(temp)
+
+        el_counter = 0  # counter for current element
+        for gr_num in self.blocks_sequence:
+            for el_num in self.elements_sequence:
+                # write symbol with current position in needed cell
+                self.list_letters[gr_num][el_num] = self.text[el_counter]
                 el_counter += 1
-            gr_counter += 1
-            temp_list[gr_num] = ''.join(str(x) for x in temp_list[gr_num])
-        return ''.join(temp_list)
+            self.list_letters[gr_num] = ''.join(self.list_letters[gr_num]) # decided to avoid string concatenation
+        return ''.join(self.list_letters)
 
 
-crypto = ReversedEncryption("012345678", "1 0 2", "1 2 0")
+message = "012345678"
+groups_seq = "1 0 2"
+elements_seq = "1 2 0"
+
+# message = "abcdefghijklmnopqrst"
+# groups_seq = "9 8 7 6 5 4 3 2 0 1"
+# elements_seq = "1 0"
+
+# message = "abcdefghijklmno"
+# groups_seq = "2 0 3 4 1"
+# elements_seq = "1 0 2"
+
+crypto = TurningEncryption(message, groups_seq, elements_seq)
 enc = crypto.encrypt()
 print(enc)
-crypto = ReversedEncryption("453120786", "1 0 2", "1 2 0")
-dec = crypto.decrypt()
-print(dec)
-
-crypto = ReversedEncryption("abcdefghijklmnopqrst", "9 8 7 6 5 4 3 2 0 1", "1 0")
-enc = crypto.encrypt()
-print(enc)
-crypto = ReversedEncryption("tsrqponmlkjihgfebadc", "9 8 7 6 5 4 3 2 0 1", "1 0")
-dec = crypto.decrypt()
-print(dec)
-
-
-crypto = ReversedEncryption("abcdefghijklmno", "2 0 3 4 1", "1 0 2")
-enc1 = crypto.encrypt()
-print(enc1)
-crypto = ReversedEncryption("hgibackjlnmoedf", "2 0 3 4 1", "1 0 2")
+crypto = TurningEncryption(enc, groups_seq, elements_seq)
 dec = crypto.decrypt()
 print(dec)
